ea8ce366722bdf00ad4ce8851a5470d620465f61
desioc <61605795+desioc@users.noreply.github.com>
2021-04-21 09:22:02 +0200

    modifica prova (test)

--- DeSyOProject/src/com/cdsc/desyo/DeSyOLauncher.java
+++ DeSyOProject/src/com/cdsc/desyo/DeSyOLauncher.java
@@ -9,6 +9,8 @@ public class DeSyOLauncher {
 		//caricare i profili (solo i primi n... da ordinare eventualmente)
 		//carico gui (passando i dati precedenti)
 		new GUI();
+		
+		int i = 0;
 	}
 
 }
