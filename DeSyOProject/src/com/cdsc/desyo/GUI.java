453da4029183a636e05df67839ea3b4701e33d6d
desioc <61605795+desioc@users.noreply.github.com>
2021-04-28 10:13:53 +0200

    Modifiche su GUI

--- DeSyOProject/src/com/cdsc/desyo/GUI.java
+++ DeSyOProject/src/com/cdsc/desyo/GUI.java
@@ -10,20 +10,23 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class GUI {
+import com.cdsc.areoswing.ActiveWall;
+import com.cdsc.areoswing.Wall;
+
+public class GUI implements ActiveWall {
 	// private ProfileManager profileManager;
 	private StartDTO startDTO;
 	// GUI
 	private JPanel p;
-	private JFrame f;
-	private JButton btnSalva;
+	private JFrame frame;
+	private JButton saveButton;
 	private JButton btnAggiungi;
 	private JButton btnScegli;
 	private JButton btnCreaProfilo;
 	private JList listBoxP;
 	private JList listBoxF;
-	private String[] ar = {"Formazione", "Produzione"};
-	
+	private String[] ar = { "Formazione", "Produzione" };
+
 	public GUI() {
 		// profileManager = new ProfileManager();
 		try {
@@ -32,49 +35,48 @@ public class GUI {
 			System.err.println(e.getMessage());
 		}
 		render();
-		 f= new JFrame ("DeSyO");
-		 p= new JPanel();
-		 btnSalva= new JButton("Salva");
-		 btnAggiungi = new JButton("Aggiungi + ");
-		 btnScegli = new JButton("Scegli");
-		 listBoxP = new JList(ar);
-		 listBoxF = new JList();
+		frame = new JFrame("DeSyO");
+		p = new JPanel();
+		saveButton = new JButton("Salva");
+		btnAggiungi = new JButton("Aggiungi + ");
+		btnScegli = new JButton("Scegli");
+		listBoxP = new JList(ar);
+		listBoxF = new JList();
+		init();
 	}
+
 	public void setup() {
-		//setup frame
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setBounds(100, 100, 759, 453);
-	
-		//setPanel
-		p.setBackground(new Color(255, 255,255));
+		// setup frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 759, 453);
+
+		// setPanel
+		p.setBackground(new Color(255, 255, 255));
 		p.setBorder(new EmptyBorder(5, 5, 5, 5));
-		p.setLayout(null);//Layout Absolute
-		f.add(p);
-		
-		
-		//setup button
-		btnSalva.setBackground(Color.LIGHT_GRAY);
-		btnSalva.setBounds(50, 300, 100, 30);
+		p.setLayout(null);// Layout Absolute
+		frame.add(p);
+
+		// setup button
+		saveButton.setBackground(Color.LIGHT_GRAY);
+		saveButton.setBounds(50, 300, 100, 30);
 		btnAggiungi.setBackground(Color.LIGHT_GRAY);
 		btnAggiungi.setBounds(450, 300, 100, 30);
 		btnScegli.setBackground(Color.LIGHT_GRAY);
 		btnScegli.setBounds(600, 300, 100, 30);
-		
-		p.add(btnSalva);
+
+		p.add(saveButton);
 		p.add(btnAggiungi);
 		p.add(btnScegli);
-		
-		//setup ListBox
+
+		// setup ListBox
 		listBoxP.setBounds(50, 50, 200, 200);
 		listBoxP.setBackground(Color.LIGHT_GRAY);
 		listBoxF.setBounds(450, 50, 255, 200);
 		listBoxF.setBackground(Color.LIGHT_GRAY);
 		p.add(listBoxP);
 		p.add(listBoxF);
-		
-		
-		
-		f.setVisible(true);
+
+		frame.setVisible(true);
 	}
 //	public List<Resource> getAllResources(Profile profile) {
 //		return profileManager.getAllResources(profile);
@@ -84,5 +86,23 @@ public class GUI {
 		System.out.println(startDTO);
 //		String profileLabel = resourceBundle.getString("gui.profile");
 //		System.out.println("Label profilo = " + profileLabel);
+	}
+
+	@Override
+	public void setLayouts() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addDetails() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void registerListeners() {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- DeSyOProject/src/com/cdsc/desyo/TestConfManager.java
+++ DeSyOProject/src/com/cdsc/desyo/TestConfManager.java
@@ -7,10 +7,6 @@ public class TestConfManager {
 	public static void main(String[] args) throws ConfigurationException {
 		Properties configuration = ConfManager.getConfiguration();
 		System.out.println(configuration);
-		
-		GUI gui = new GUI();
-		gui.setup();
-		
 	}
 
 }
