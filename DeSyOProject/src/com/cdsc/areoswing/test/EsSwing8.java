e4e0c57cff5e55e4d879f47bfe3ed509f7a73c9a
AndreaGuida73 <andrea.guida@email.it>
2021-04-29 22:21:00 +0200

    EsSwing test

---
+++ DeSyOProject/src/com/cdsc/areoswing/test/EsSwing8.java
@@ -0,0 +1,13 @@
+package com.cdsc.areoswing.test;
+
+import javax.swing.JFrame;
+
+import com.cdsc.areoswing.Es8Panel;
+public class EsSwing8 {
+public static void main(String[] args) {
+JFrame f = new JFrame("Esempio 8");
+Es8Panel p = new Es8Panel();
+f.add(p);
+f.pack();
+f.setVisible(true);
+}}
